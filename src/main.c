15b38e341ea6e6ee27bc5459ffdf18a9cbec9265
John Doe <johndoe@example.com>
2024-03-30 22:56:33 -0400

    init main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,45 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
 
 
-int main(int argc, char const *argv[]){
-    
+int main( int argc, char* argv[] ) {
+
+  if (argc != 1) {
+    // TODO: logic for setting brightness based on a percentage, specified in arguments.
     return 0;
+  }
+
+  int c;
+  static struct termios old_t, new_t;
+  tcgetattr( STDIN_FILENO, &old_t);
+  new_t = old_t;
+  new_t.c_lflag &= ~(ICANON);
+  new_t.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr( STDIN_FILENO, TCSANOW, &new_t);
+  
+  puts("w -- increase brightness");
+  puts("s -- decrease brightness");
+  puts("q -- quit");
+
+  while( 1 ) {
+    c = getchar();
+    switch (c) {
+      
+      case 'q':
+        tcsetattr( STDIN_FILENO, TCSANOW, &old_t);
+        return 0;
+        break;
+      case 'w':
+        // TODO: logic for increasing brightness.
+        break;
+      case 's':
+        // TODO: logic for decreasing brightness.
+        break;
+
+    }
+  }
+
+  return 0;
+
 }
